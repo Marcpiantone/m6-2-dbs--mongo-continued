c29ed4b8ba7582ebae949081aeff17513d48b822
“Marc <marc_piantone@live.fr>
2020-08-18 20:11:05 -0400

    Answer exercise-3

--- server/handlers.js
+++ server/handlers.js
@@ -10,38 +10,6 @@ const options = {
   useUnifiedTopology: true,
 };
 
-const NUM_OF_ROWS = 8;
-const SEATS_PER_ROW = 12;
-
-const getRowName = (rowIndex) => {
-  return String.fromCharCode(65 + rowIndex);
-};
-
-const randomlyBookSeats = (num) => {
-  const bookedSeats = {};
-
-  while (num > 0) {
-    const row = Math.floor(Math.random() * NUM_OF_ROWS);
-    const seat = Math.floor(Math.random() * SEATS_PER_ROW);
-
-    const seatId = `${getRowName(row)}-${seat + 1}`;
-
-    bookedSeats[seatId] = true;
-
-    num--;
-  }
-
-  return bookedSeats;
-};
-
-let state;
-
-if (!state) {
-  state = {
-    bookedSeats: randomlyBookSeats(30),
-  };
-}
-
 const getSeats = async (req, res) => {
   const client = await MongoClient(MONGO_URI, options);
 
@@ -58,12 +26,11 @@ const getSeats = async (req, res) => {
         result.forEach((seat) => (formattedSeats[seat._id] = seat));
         res.status(200).json({
           seats: formattedSeats,
-          bookedSeats: state.bookedSeats,
           numOfRows: 8,
           seatsPerRow: 12,
         });
       } else {
-        res.status(404).json({ data: "Not found, 404" });
+        res.status(404).json({ data: "Seats not found, 404" });
       }
     });
   } catch (err) {
@@ -73,4 +40,49 @@ const getSeats = async (req, res) => {
   client.close();
 };
 
-module.exports = { getSeats };
+const handleBooking = async (req, res) => {
+  const { seatId, creditCard, expiration } = req.body;
+  const client = await MongoClient(MONGO_URI, options);
+
+  const _id = seatId;
+
+  const query = { _id };
+  const newValues = { $set: { isBooked: true } };
+
+  console.log(query);
+  console.log(newValues);
+
+  if (!creditCard || !expiration) {
+    return res.status(400).json({
+      status: 400,
+      message: "Please provide credit card information!",
+    });
+  }
+
+  try {
+    await client.connect();
+    const db = client.db("m6-2");
+    const collection = db.collection("seats");
+
+    const seat = await collection.findOne({ _id });
+    const bookingStatus = seat.isBooked;
+
+    if (bookingStatus === false) {
+      const r = await collection.updateOne(query, newValues);
+      assert.equal(1, r.matchedCount);
+      assert.equal(1, r.modifiedCount);
+      return res.status(200).json({
+        status: 200,
+        success: true,
+      });
+    } else {
+      return res
+        .status(400)
+        .json({ message: "This seat has already been booked!" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+module.exports = { getSeats, handleBooking };
